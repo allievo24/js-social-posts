d2ffbcf0e2a9c06358d87f64ff6d405385914307
saidmoussadek <saiddus24@gmail.com>
2022-07-25 00:36:28 +0200

    push correzione

---
+++ js/main.js
@@ -0,0 +1,158 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+
+
+const containerDom = document.getElementById("container");
+const likeButtonDom = document.getElementsByClassName("js-like-button")
+const likeCounter  = document.getElementsByClassName("js-likes-counter")
+const likeArray =[];
+
+
+for( let i=0; i<posts.length; i++){
+    creaPost(posts,i);
+    
+
+
+
+
+}
+
+
+
+/********************************************Approccio con data attribute
+
+for( let i=0; i<likeButtonDom.length; i++){
+        likeButtonDom[i].addEventListener('click',function(event){
+        event.preventDefault();
+        this.classList.add('like-button--liked');//al elemeznto che subisce il click aggiungo una class
+        const postid = this.getAttribute('data-postid');//recupero l'attributo postid 
+        const likeCounter = document.getElementById("like-counter-" + postid);//recupero elemento dal dom sul e gli aggiungo il postid recuprato prima
+        likeCounter.innerText = parseInt (likeCounter.innerText) + 1;//al'elemento recuperato che nel dom rapresenta il numero di like si incrementa di 1 ad ogni click
+    
+        
+        
+    });
+
+}
+******************************************************************************/
+
+for(let i=0; i<likeButtonDom.length; i++ ){
+    likeButtonDom[i].addEventListener('click',function(event){
+        event.preventDefault();
+        likeButtonDom[i].classList.add('like-button--liked');
+        likeCounter[i].innerText =  parseInt(likeCounter[i].innerText) +  1
+        likeArray.push(posts[i].id)
+        console.log(likeArray)
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function creaPost(posts,i){
+        containerDom.innerHTML += `<div>
+                                        <div class="post">
+                                        <div class="post__header">
+                                            <div class="post-meta">                    
+                                                <div class="post-meta__icon">
+                                                    <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.image}"/>                    
+                                                </div>
+                                                <div class="post-meta__data">
+                                                    <div class="post-meta__author">${posts[i].author.name}</div>
+                                                    <div class="post-meta__time">${posts[i].created}</div>
+                                                </div>                    
+                                            </div>
+                                        </div>
+                                        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+                                        <div class="post__image">
+                                            <img src="${posts[i].media}"alt=""/>
+                                        </div>
+                                        <div class="post__footer">
+                                            <div class="likes js-likes">
+                                                <div class="likes__cta">
+                                                    <a class="like-button   js-like-button" href="#" data-postid="${posts[i].id}">
+                                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                        <span class="like-button__label">Mi Piace</span>
+                                                    </a>
+                                                </div>
+                                                <div class="likes__counter">
+                                                    Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                                                </div>
+                                            </div> 
+                                        </div>            
+                                        </div>
+                                    </div>`
+
+};
+
